96072d52f7b1b0c51cacd7b10780b5f04d4a823f
micahpf <micahpfletcher@gmail.com>
2025-10-24 15:23:18 +0100

    Move the for loop for categorizing columns to a new function categorize_cols(). This means it can be called separately for testing and generating example function inputs.

--- easyplater_functions.r
+++ easyplater_functions.r
@@ -65,41 +65,9 @@ get_and_format_plate_df_from_manifest <- function(manifest_df, plateID, columns_
   num_internal_controls <- length(internal_control_well_indices)
   real_plate_size <- plate_size - num_internal_controls
   
-  if(length(cols_to_categorize)>0){ #cols_to_categorize is list of tuples with structure (col_name,num_cats,na_replacement,categorized_col_name)
-    for(col_to_categorize_tuple in cols_to_categorize){
-      
-      col_name <- col_to_categorize_tuple[1]
-      num_cats <- as.numeric(col_to_categorize_tuple[2])
-      
-      cut_interval_has_worked = FALSE
-      while((!cut_interval_has_worked) & num_cats > 0){
-      tryCatch(
-        {
-          categorized_col_vec <- as.numeric(cut_interval(as.matrix(plate_df_aux[,col_name]),num_cats))
-          cut_interval_has_worked <- TRUE
-          #print(paste0("Final num_cats: ", num_cats))
-          
-        }, error = function(msg){
-        }, warning = function(msg){
-        })
-        num_cats <- num_cats - 1
-      }
-      if(!cut_interval_has_worked){
-        categorized_col_vec <- plate_df_aux[,col_name]
-      }
-      
-      if(length(col_to_categorize_tuple)==4){
-        na_replacement <- col_to_categorize_tuple[3]
-        categorized_col_name <- col_to_categorize_tuple[4]
-        replace(categorized_col_vec, is.na(categorized_col_vec), na_replacement)
-      }else{
-        categorized_col_name <- col_to_categorize_tuple[3]
-      }
-
-      plate_df_aux[paste(categorized_col_name)] <- categorized_col_vec
-      #print(categorized_col_name)
-    }
-  }
+  # Categorize columns as specified by list of vectors in columns_for_scoring
+  # # cols_to_categorize is list of character vectors with structure (col_name,num_cats,na_replacement,categorized_col_name)
+  plate_df_aux <- categorize_cols(plate_df_aux, columns_for_scoring)
 
   plate_df <- select(plate_df_aux, cols_for_analysis)
   plate_df_aux <- select(plate_df_aux, c(cols_for_analysis, c("plate","column","row","well")))
@@ -178,6 +146,44 @@ get_and_format_plate_df_from_manifest <- function(manifest_df, plateID, columns_
   return(list(plate_df, plate_df_aux))
 }
 
+categorize_cols <- function(plate_df_aux, cols_to_categorize) {
+  # cols_to_categorize is list of tuples with structure (col_name,num_cats,na_replacement,categorized_col_name)
+  if(length(cols_to_categorize)>0){ 
+    for(col_to_categorize_tuple in cols_to_categorize){
+      
+      col_name <- col_to_categorize_tuple[1]
+      num_cats <- as.numeric(col_to_categorize_tuple[2])
+      
+      cut_interval_has_worked = FALSE
+      while((!cut_interval_has_worked) & num_cats > 0){
+        tryCatch(
+          {
+            categorized_col_vec <- as.numeric(cut_interval(as.matrix(plate_df_aux[,col_name]),num_cats))
+            cut_interval_has_worked <- TRUE
+            #print(paste0("Final num_cats: ", num_cats))
+            
+          }, error = function(msg){
+          }, warning = function(msg){
+          })
+        num_cats <- num_cats - 1
+      }
+      if(!cut_interval_has_worked){
+        categorized_col_vec <- plate_df_aux[,col_name]
+      }
+      
+      if(length(col_to_categorize_tuple)==4){
+        na_replacement <- col_to_categorize_tuple[3]
+        categorized_col_name <- col_to_categorize_tuple[4]
+        replace(categorized_col_vec, is.na(categorized_col_vec), na_replacement)
+      }else{
+        categorized_col_name <- col_to_categorize_tuple[3]
+      }
+      
+      plate_df_aux[paste(categorized_col_name)] <- categorized_col_vec
+      #print(categorized_col_name)
+    }
+  } 
+}
 
 #*******************************************************************************
 make_ss_matrices <- function(plate_df, column_weights, imbalance_fixer, plate_size){
