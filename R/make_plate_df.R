97b8ddf0e1be65e950e68a406b9774d773fc772a
micahpf <micahpfletcher@gmail.com>
2025-11-06 14:35:08 +0000

    Write (but don't document or export) make_easyplater_design_aux()

--- R/make_plate_df.R
+++ R/make_plate_df.R
@@ -163,3 +163,23 @@ get_and_format_plate_df_from_manifest <- function(manifest_df, plateID, columns_
 
   return(list(plate_df, plate_df_aux))
 }
+
+# Take plate_df_list and return the same with samples reordered by samples_final_order
+# Micah: A clearer name may be apply_final_well_locations()
+make_easyplater_design_aux <- function(plate_df_list, samples_final_order){
+
+  plate_df_aux <- plate_df_list[[2]]
+
+  # Need to reset rows and columns (otherwise nothing changes!)
+  plate_rows <- plate_df_aux$row
+  plate_columns <- plate_df_aux$column
+  plate_wells <- plate_df_aux$well
+
+  plate_design_df <- plate_df_aux |> dplyr::slice(match(samples_final_order, .data$SampleID))
+
+  plate_design_df$row <- plate_rows
+  plate_design_df$column <- plate_columns
+  plate_design_df$well <- plate_wells
+
+  return(plate_design_df)
+}
