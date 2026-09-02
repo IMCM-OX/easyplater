ccd070c4c019dbea8f302d537345dbc33461e0bb
micahpf <micahpfletcher@gmail.com>
2026-09-02 18:15:28 +0100

    Update text fixtures to reflect renaming of Empty wells with well ID rather than arbitrary number.

--- tests/testthat/test-get_and_format_plate_df_from_manifest.R
+++ tests/testthat/test-get_and_format_plate_df_from_manifest.R
@@ -1,36 +1,28 @@
-test_that("get_and_format_plate_df_from_manifest() returns the original example plate_df dataframe", {
-  expect_identical(
-    object = {
-      manifest_df <- readRDS(test_path("fixtures", "manifest_df.rds"))
-      get_and_format_plate_df_from_manifest(
-        manifest_df = manifest_df,
-        plateID = "plate 1",
-        columns_for_scoring = c("Cohort", "Group", "Sex", "AgeGroup"),
-        cols_to_categorize = list(c("Age", "10", "AgeGroup")),
-        plate_size = 96,
-        plate_wells = paste0(rep(LETTERS[1:8], times = 12), rep(1:12, each = 8)),
-        internal_control_well_indices = 86:95,
-        internal_control_ids = c(paste0("SC", 1:2), paste0("NC", 1:3), paste0("PC", 1:5))
-      )[[1]]
-    },
-    expected = readRDS(test_path("fixtures", "plate_df.rds"))
-  )
-})
-
-test_that("get_and_format_plate_df_from_manifest() returns the original example plate_df_aux dataframe", {
+test_that("make_plate_df() returns the original example plate_df_aux dataframe", {
   expect_equal(
     object = {
       manifest_df <- readRDS(test_path("fixtures", "manifest_df.rds"))
-      get_and_format_plate_df_from_manifest(
-        manifest_df = manifest_df,
-        plateID = "plate 1",
-        columns_for_scoring = c("Cohort", "Group", "Sex", "AgeGroup"),
-        cols_to_categorize = list(c("Age", "10", "AgeGroup")),
-        plate_size = 96,
-        plate_wells = paste0(rep(LETTERS[1:8], times = 12), rep(1:12, each = 8)),
-        internal_control_well_indices = 86:95,
-        internal_control_ids = c(paste0("SC", 1:2), paste0("NC", 1:3), paste0("PC", 1:5))
-      )[[2]]
+
+      # plate_col <- "plate"
+      imbalance_fixer <- FALSE
+      plate_num_rows <- 8
+      plate_num_cols <- 12
+      plate_wells <- paste0(rep(LETTERS[1:plate_num_rows], times = plate_num_cols),
+                            rep(1:plate_num_cols, each = plate_num_rows))
+
+      n_samples_plate1 <- sum(manifest_df$plate == "plate 1") # 81
+      olink_ht_ic_labels <- c(paste0("SC", 1:2), paste0("NC", 1:3), paste0("PC", 1:5))
+      fixed_wells <- assign_fixed_wells(n_samples_plate1, 87:96, olink_ht_ic_labels)
+
+      manifest_df_cut <- manifest_df |>
+        dplyr::mutate(AgeGroup = ggplot2::cut_interval(Age, 10) |> as.numeric(),
+                      .by = "plate") |>
+        dplyr::relocate(AgeGroup, .after = "Age")
+
+      sample_df <- manifest_df_cut |> dplyr::filter(.data[["plate"]] == "plate 1")
+      # Compare to original plate_df_aux
+      make_plate_df(sample_df, fixed_wells, imbalance_fixer, plate_wells) |>
+        dplyr::select(-Age)
     },
     expected = readRDS(test_path("fixtures", "plate_df_aux.rds"))
     )
