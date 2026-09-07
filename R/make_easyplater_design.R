9e6e0031ad0e5fa30c589ee3ebd4157f2660e538
micahpf <micahpfletcher@gmail.com>
2026-09-07 11:03:18 +0100

    Remove technical internal variables from make_easyplater_design() as requested in 430ae8c

--- R/make_easyplater_design.R
+++ R/make_easyplater_design.R
@@ -19,9 +19,6 @@ SampleID <- NULL
 #' @param fixed_wells Data frame output by [easyplater::assign_fixed_wells]. If this argument is used, `internal_control_well_indices` and `internal_control_ids` will be ignored.
 #' @param internal_control_well_indices (Deprecated) Numeric vector containing indices of control wells. Expecting zero index, and numbering going first top to bottom, then left to right. This argument is ignored if `fixed_wells` argument is used.
 #' @param internal_control_ids (Deprecated) Character vector. Names of internal control wells. This argument is ignored if `fixed_wells` argument is used.
-#' @param full_mask `nrow(plate_df) x nrow(plate_df)` numeric matrix. **TO DO: Micah, please hide this technical, internal variable that needs to be hidden/ de-surfaced from the user.**
-#' @param scoring_mask `nrow(plate_df) x nrow(plate_df)` numeric matrix. **TO DO: Micah, please hide this technical, internal variable that needs to be hidden/ de-surfaced from the user**
-#' @param well_pair_distances_df **TO DO: Micah, please hide this technical, internal variable that needs to be hidden/ de-surfaced from the user**
 #' @param splitting_ss_thresh Numeric scalar. Default: 0.5. Sample similarity (ss) threshold. Used to identify pairs of similar samples, i.e., whose pairwise similarity is greater than this threshold; if found to be in nearby wells, these samples might be moved into distal wells (i.e. split apart) to potentially improve the plate design. See <a href='https://arxiv.org/abs/2512.17988'>\[1\]</a>; section 2.2, step 3. See also Supplementary Figure 4.
 #' @param splitting_wd_thresh Numeric scalar. Default: 1. Well distance (wd) threshold. Used to identify pairs of wells which are nearby to one another, i.e., whose distance is less than this threshold. See <a href='https://arxiv.org/abs/2512.17988'>\[1\]</a>; section 2.2, step 3. See also Supplementary FIgure 4.
 #' @param replacing_ss_thresh Numeric scalar. Default: 0.5. Sample similarity (ss) threshold. Used to identify pairs of dissimilar samples, i.e., whose pairwise similarity is less than or equal to this threshold; if found to be in distal wells, these samples might be switched with a sample that is under consideration for a split. See <a href='https://arxiv.org/abs/2512.17988'>\[1\]</a>; section 2.2, step 3. See also Supplementary Figure 4.
@@ -94,8 +91,6 @@ make_easyplater_design <- function(manifest_df, plateID = NULL,
                                    fixed_wells = NULL,
                                    internal_control_well_indices = NULL,
                                    internal_control_ids = NULL,
-                                   full_mask = NULL, scoring_mask = NULL,
-                                   well_pair_distances_df = NULL,
                                    splitting_ss_thresh = 0.5, splitting_wd_thresh = 1,
                                    replacing_ss_thresh = 0.5, replacing_wd_thresh = 6,
                                    max_depth = 2, wins_required = 10, max_attempts = 100,
@@ -120,19 +115,10 @@ make_easyplater_design <- function(manifest_df, plateID = NULL,
   plate_wells <- paste0(rep(LETTERS[1:plate_num_rows], times = plate_num_cols),
                         rep(1:plate_num_cols, each = plate_num_rows))
 
+  well_pair_distances_df <- make_well_distance_df(plate_size)
   well_distances_matrix <- make_well_distances_matrix(plate_size)
-
-  if (is.null(full_mask)) {
-    full_mask <- make_full_mask(well_distances_matrix)
-  }
-
-  if (is.null(scoring_mask)) {
-    scoring_mask <- make_scoring_mask(well_distances_matrix)
-  }
-
-  if (is.null(well_pair_distances_df)) {
-    well_pair_distances_df <- make_well_distance_df(plate_size)
-  }
+  full_mask <- make_full_mask(well_distances_matrix)
+  scoring_mask <- make_scoring_mask(well_distances_matrix)
 
   # Check that fixed_wells has a plate_col column
   if (!is.null(fixed_wells)) {
